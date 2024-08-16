3c3616baf098e3ec5fde8b91552e0b4319ec956d
loganrun <bigdog_41@hotmai.com>
2024-08-16 10:36:20 -0700

    not working

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,59 +1,14 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
-//import { useQuill } from 'react-quilljs';
-// import VoiceChat from '../components/VoiceChat'
-//import 'quill/dist/quill.snow.css';
+import React, { useEffect, useRef } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
-//import io from 'socket.io-client';
 import useSocket from '../components/UseSocket';
 
-function TextEditor({id}) {
-  //console.log(id)
-  //const documentId = id
-  //const socket = useSocket('https://www.2sigmasolution.com');
-  //console.log(roomId)
-  //const tutor = useSelector(state => state.tutorauth.tutor.payload.tutor); 
-  //const roomId  = tutor.roomId
-  //console.log(roomId);
-  //const userName = tutor.userName 
-  //console.log(userName);
-
-  //const SAVE_INTERVAL_MS = 2000
-
+function TextEditor({ id }) {
   const documentId = id;
   const socket = useSocket('https://www.2sigmasolution.com');
   const SAVE_INTERVAL_MS = 2000;
   const quillRef = useRef(null);
   const editorRef = useRef(null);
-
-
-  // useEffect(() => {
-  //   if (editorRef.current === null) return;
-
-  //   const quill = new Quill(editorRef.current, {
-  //     theme: 'snow',
-  //     modules: {
-  //       toolbar: [
-  //         ['bold', 'italic', 'underline', 'strike'],
-  //         [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-  //         [{ 'indent': '-1'}, { 'indent': '+1' }],
-  //         [{ 'direction': 'rtl' }],
-  //         [{ 'size': ['small', false, 'large', 'huge'] }],
-  //         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  //         [{ 'color': [] }, { 'background': [] }],
-  //         [{ 'font': [] }],
-  //         [{ 'align': [] }],
-  //         ['clean']
-  //       ]
-  //     }
-  //   });
-
-  //   quillRef.current = quill;
-
-  //   return () => {
-  //     quillRef.current = null;
-  //   };
-  // }, []);
 
   useEffect(() => {
     if (editorRef.current === null) return;
@@ -104,7 +59,6 @@ function TextEditor({id}) {
     };
   }
 
-
   useEffect(() => {
     if (socket == null || quillRef.current == null) return;
 
@@ -149,15 +103,123 @@ function TextEditor({id}) {
   }, []);
 
   return (
-    <div className="container mx-auto p-4 h-screen ">
-    <h1 className="text-2xl font-bold mb-4">Tutoring Session</h1>
-    <div ref={editorRef} className="h-full bg-white text-black shadow rounded "></div>
-  
+    <div className="container mx-auto p-4 h-screen">
+      <h1 className="text-2xl font-bold mb-4">Tutoring Session</h1>
+      <div ref={editorRef} className="h-full bg-white text-black shadow rounded"></div>
     </div>
   );
 }
 
 export default TextEditor;
 
+// function TextEditor({ id }) {
+//   const documentId = id;
+//   const socket = useSocket('https://www.2sigmasolution.com');
+//   const SAVE_INTERVAL_MS = 2000;
+//   const quillRef = useRef(null);
+//   const editorRef = useRef(null);
 
+//   useEffect(() => {
+//     if (editorRef.current === null) return;
 
+//     // Only initialize Quill once
+//     if (quillRef.current === null) {
+//       const quill = new Quill(editorRef.current, {
+//         theme: 'snow',
+//         modules: {
+//           toolbar: [
+//             ['bold', 'italic', 'underline', 'strike'],
+//             [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+//             [{ 'indent': '-1'}, { 'indent': '+1' }],
+//             [{ 'direction': 'rtl' }],
+//             [{ 'size': ['small', false, 'large', 'huge'] }],
+//             [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+//             [{ 'color': [] }, { 'background': [] }],
+//             [{ 'font': [] }],
+//             [{ 'align': [] }],
+//             ['clean']
+//           ],
+//         },
+//       });
+
+//       //quill.getModule('toolbar').addHandler('image', imageHandler);
+//       quillRef.current = quill;
+//     }
+
+//     return () => {
+//       quillRef.current = null;
+//     };
+//   }, []);
+
+//   // Image handler function (you can customize this as needed)
+//   // function imageHandler() {
+//   //   const input = document.createElement('input');
+//   //   input.setAttribute('type', 'file');
+//   //   input.setAttribute('accept', 'image/*');
+//   //   input.click();
+
+//   //   input.onchange = () => {
+//   //     const file = input.files[0];
+//   //     if (file) {
+//   //       const reader = new FileReader();
+//   //       reader.onload = (e) => {
+//   //         const range = quillRef.current.getSelection(true);
+//   //         quillRef.current.insertEmbed(range.index, 'image', e.target.result);
+//   //       };
+//   //       reader.readAsDataURL(file);
+//   //     }
+//   //   };
+//   // }
+
+//   useEffect(() => {
+//     if (socket == null || quillRef.current == null) return;
+
+//     const quill = quillRef.current;
+
+//     socket.once("load-document", document => {
+//       quill.setContents(document);
+//       quill.enable();
+//     });
+
+//     socket.emit("get-document", documentId);
+
+//     const interval = setInterval(() => {
+//       socket.emit("save-document", quill.getContents());
+//     }, SAVE_INTERVAL_MS);
+
+//     const handler = (delta, oldDelta, source) => {
+//       if (source !== "user") return;
+//       socket.emit("send-changes", delta);
+//     };
+
+//     quill.on("text-change", handler);
+
+//     const receiveHandler = delta => {
+//       quill.updateContents(delta);
+//     };
+
+//     socket.on("receive-changes", receiveHandler);
+
+//     return () => {
+//       clearInterval(interval);
+//       quill.off("text-change", handler);
+//       socket.off("receive-changes", receiveHandler);
+//     };
+//   }, [socket, documentId]);
+
+//   useEffect(() => {
+//     const toolbar = document.querySelector('.ql-toolbar');
+//     if (toolbar) {
+//       toolbar.classList.add('bg-white', 'text-black');
+//     }
+//   }, []);
+
+//   return (
+//     <div className="container mx-auto p-4 h-screen ">
+//       <h1 className="text-2xl font-bold mb-4">Tutoring Session</h1>
+//       <div ref={editorRef} className="h-full bg-white text-black shadow rounded"></div>
+//     </div>
+//   );
+// }
+
+// export default TextEditor;
