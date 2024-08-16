22558276181422f275c95e5436b40649eb245da8
loganrun <bigdog_41@hotmai.com>
2024-08-16 10:06:52 -0700

    toolbar issue

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,103 +1,157 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react';
-import { useQuill } from 'react-quilljs';
+//import { useQuill } from 'react-quilljs';
 // import VoiceChat from '../components/VoiceChat'
+//import 'quill/dist/quill.snow.css';
+import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
-import io from 'socket.io-client';
+//import io from 'socket.io-client';
 import useSocket from '../components/UseSocket';
 
 function TextEditor({id}) {
   //console.log(id)
-  const documentId = id
-  const socket = useSocket('https://www.2sigmasolution.com');
+  //const documentId = id
+  //const socket = useSocket('https://www.2sigmasolution.com');
   //console.log(roomId)
   //const tutor = useSelector(state => state.tutorauth.tutor.payload.tutor); 
-    //const roomId  = tutor.roomId
-    //console.log(roomId);
-    //const userName = tutor.userName 
-    //console.log(userName);
+  //const roomId  = tutor.roomId
+  //console.log(roomId);
+  //const userName = tutor.userName 
+  //console.log(userName);
 
-    const SAVE_INTERVAL_MS = 2000
+  //const SAVE_INTERVAL_MS = 2000
 
-  const modules = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-      [{ 'direction': 'rtl' }],                         // text direction
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-      [{ 'font': [] }],
-      [{ 'align': [] }],
-      ['clean']                                         // remove formatting button
-    ]
-  };
+  const documentId = id;
+  const socket = useSocket('https://www.2sigmasolution.com');
+  const SAVE_INTERVAL_MS = 2000;
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
 
-  const { quill, quillRef: editorContainerRef } = useQuill({ modules, theme: 'snow' });
+
+  // useEffect(() => {
+  //   if (editorRef.current === null) return;
+
+  //   const quill = new Quill(editorRef.current, {
+  //     theme: 'snow',
+  //     modules: {
+  //       toolbar: [
+  //         ['bold', 'italic', 'underline', 'strike'],
+  //         [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+  //         [{ 'indent': '-1'}, { 'indent': '+1' }],
+  //         [{ 'direction': 'rtl' }],
+  //         [{ 'size': ['small', false, 'large', 'huge'] }],
+  //         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  //         [{ 'color': [] }, { 'background': [] }],
+  //         [{ 'font': [] }],
+  //         [{ 'align': [] }],
+  //         ['clean']
+  //       ]
+  //     }
+  //   });
+
+  //   quillRef.current = quill;
+
+  //   return () => {
+  //     quillRef.current = null;
+  //   };
+  // }, []);
 
   useEffect(() => {
-    if (socket == null || quill == null) return
+    if (editorRef.current === null) return;
 
-  
-    const toolbar = document.querySelector('.ql-toolbar');
-      if (toolbar) {
-        toolbar.classList.add('bg-white', 'text-black');
-    }
+    // Create toolbar container
+    const toolbarContainer = document.createElement('div');
+    editorRef.current.appendChild(toolbarContainer);
+
+    const quill = new Quill(editorRef.current, {
+      theme: 'snow',
+      modules: {
+        toolbar: {
+          container: toolbarContainer,
+          handlers: {
+            // Add any custom handlers here
+          }
+        },
+      }
+    });
+
+    // Set up toolbar options
+    quill.getModule('toolbar').addHandler('image', imageHandler);
+
+    quillRef.current = quill;
+
+    return () => {
+      quillRef.current = null;
+    };
+  }, []);
+
+  // Image handler function (you can customize this as needed)
+  function imageHandler() {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = () => {
+      const file = input.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const range = quillRef.current.getSelection(true);
+          quillRef.current.insertEmbed(range.index, 'image', e.target.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+  }
+
+
+  useEffect(() => {
+    if (socket == null || quillRef.current == null) return;
+
+    const quill = quillRef.current;
 
     socket.once("load-document", document => {
-      //console.log("loading docs")
-      quill.setContents(document)
-      quill.enable()
-    })
+      quill.setContents(document);
+      quill.enable();
+    });
 
-    socket.emit("get-document", documentId)
-  }, [socket, quill, documentId])
-
-  useEffect(() => {
-    if (socket == null || quill == null) return
+    socket.emit("get-document", documentId);
 
     const interval = setInterval(() => {
-      socket.emit("save-document", quill.getContents())
-    }, SAVE_INTERVAL_MS)
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [socket, quill])
-
-  useEffect(() => {
-    if (socket == null || quill == null) return
-
-    const handler = delta => {
-      quill.updateContents(delta)
-    }
-    socket.on("receive-changes", handler)
-
-    return () => {
-      socket.off("receive-changes", handler)
-    }
-  }, [socket, quill])
-
-  useEffect(() => {
-    if (socket == null || quill == null) return
+      socket.emit("save-document", quill.getContents());
+    }, SAVE_INTERVAL_MS);
 
     const handler = (delta, oldDelta, source) => {
-      if (source !== "user") return
-      socket.emit("send-changes", delta)
-    }
-    quill.on("text-change", handler)
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+
+    quill.on("text-change", handler);
+
+    const receiveHandler = delta => {
+      quill.updateContents(delta);
+    };
+
+    socket.on("receive-changes", receiveHandler);
 
     return () => {
-      quill.off("text-change", handler)
-    }
-  }, [socket, quill])
+      clearInterval(interval);
+      quill.off("text-change", handler);
+      socket.off("receive-changes", receiveHandler);
+    };
+  }, [socket, documentId]);
 
-  //const roomUrl = window.location.href;
+  useEffect(() => {
+    const toolbar = document.querySelector('.ql-toolbar');
+    if (toolbar) {
+      toolbar.classList.add('bg-white', 'text-black');
+    }
+  }, []);
 
   return (
     <div className="container mx-auto p-4 h-screen ">
     <h1 className="text-2xl font-bold mb-4">Tutoring Session</h1>
-    <div ref={editorContainerRef} className="h-full bg-white text-black shadow rounded "></div>
+    <div ref={editorRef} className="h-full bg-white text-black shadow rounded "></div>
   
     </div>
   );
