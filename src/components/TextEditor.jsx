53cbd2eb33c4048803ed0190fda0d69e25e0b16c
loganrun <bigdog_41@hotmai.com>
2024-12-02 19:15:47 -0800

    dev

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -9,7 +9,7 @@ function TextEditor({ id, title }) {
   //const purpose1= purpose
   //console.log(id)
   const documentId = id;
-  const socket = useSocket('https://www.2sigmasolution.com');
+  const socket = useSocket('http://localhost:4000');
   const editorContainerRef = useRef(null);
   const quillRef = useRef(null);
 
