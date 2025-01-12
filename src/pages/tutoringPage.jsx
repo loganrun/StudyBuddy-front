4ba9082a426efeed4b5b98d73e32617f0b6291c2
loganrun <bigdog_41@hotmai.com>
2025-01-12 11:30:57 -0800

    whiteboard added

--- src/pages/tutoringPage.jsx
+++ src/pages/tutoringPage.jsx
@@ -5,6 +5,7 @@ import StartButton from '../components/modal/StartButton';
 import { useParams,useLocation } from 'react-router-dom';
 import {Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle,} from "../components/Card";
 import {Tabs,TabsContent,TabsList,TabsTrigger,} from "../components/Tabs";
+import Whiteboard from '../components/WhiteBoard';
 //import { useSelector } from 'react-redux';
 import TextEditor from '../components/TextEditor'
 //import VoiceChat from '../components/VoiceChat'
@@ -63,6 +64,13 @@ function tutoringPage({socket}) {
                     </CardContent>
                   </Card>
                 </TabsContent>
+                <TabsContent value="WhiteBoard" >
+                  <Card className="border-0 ">
+                    <CardContent>
+                    <Whiteboard socket={socket} id={documentId} roomId={roomId} />
+                    </CardContent>
+                  </Card>
+                </TabsContent>
               </Tabs>
               <div className="space-y-4">
               <VideoChat roomID={roomId} userId={id} userType={userType} isVideoOn={isVideoOn} isMuted={isMuted} isCalling={isCalling}/> 
@@ -88,9 +96,26 @@ function tutoringPage({socket}) {
         <div className="relative min-h-screen p-4">
           <div className="container mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-2">
-              <div>
-              <TextEditor socket={socket} id={documentId} roomId={roomId} />
-              </div>
+            <Tabs defaultValue='TextEditor' >
+                <TabsList className="grid w-full grid-cols-2">
+                  <TabsTrigger value="WhiteBoard">WhiteBoard</TabsTrigger>
+                  <TabsTrigger value="TextEditor">Text Editor</TabsTrigger>
+                </TabsList>
+                <TabsContent value="TextEditor" >
+                  <Card className="border-0 ">
+                    <CardContent>
+                    <TextEditor socket={socket} id={documentId} purpose={'Tutoring Session'} />
+                    </CardContent>
+                  </Card>
+                </TabsContent>
+                <TabsContent value="WhiteBoard" >
+                  <Card className="border-0 ">
+                    <CardContent>
+                    <Whiteboard socket={socket} id={documentId} roomId={roomId} />
+                    </CardContent>
+                  </Card>
+                </TabsContent>
+              </Tabs>
               <div className="space-y-4">
                 <VideoChat roomID={roomId} userId={id} isVideoOn={isVideoOn} isMuted={isMuted} isCalling={isCalling}/>             
               <OpenAiInterface/>
